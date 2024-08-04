542612aaece6140b754e142930f62838fbe6869d
Brian Rivera Martinez <contactbrianriveramartinez@gmail.com>
2024-08-04 17:25:01 -0600

    Visual-Block configuration

--- lua/options.lua
+++ lua/options.lua
@@ -13,6 +13,7 @@ vim.opt.relativenumber = true
 
 vim.keymap.set('n', 'j', 'gj', { noremap = true, silent = true })
 vim.keymap.set('n', 'k', 'gk', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-z>', '<C-v>', { noremap = true, silent = true })
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
